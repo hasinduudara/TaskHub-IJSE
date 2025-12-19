e353f335b74d87fbdec8fee8e6b9218f55110309
hasinduudara <hasindutechschool@gmail.com>
2025-12-19 13:30:29 +0530

    Add button to navigate to task form from main screen

--- app/(dashboard)/tasks/index.tsx
+++ app/(dashboard)/tasks/index.tsx
@@ -1,12 +1,23 @@
-import {View, Text} from 'react-native'
+import {View, Text, TouchableOpacity} from 'react-native'
 import React from 'react'
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import {useRouter} from "expo-router";
 
 const Index = () => {
+    const router = useRouter();
     return (
         <View className="flex-1 justify-center items-center">
+
             <Text className="text-3xl font-bold text-gray-800 text-center">
                 Tasks
             </Text>
+
+            <TouchableOpacity
+                className="absolute bottom-10 right-10"
+                onPress={() => router.push('/tasks/form')}
+            >
+                <MaterialIcons name="add-circle-outline" size={50} color="#4F46E5" />
+            </TouchableOpacity>
         </View>
     )
 }
